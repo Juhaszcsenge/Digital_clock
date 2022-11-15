d28c809d4335bda8960b1178a13fd9ecf9068497
Juhaszcsenge <96292546+Juhaszcsenge@users.noreply.github.com>
2022-11-15 12:03:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
+import './style.css';
 
-function updateClock() {
-  let h = new Date().getHours();
-  let m = new Date().getMinutes();
-  let s = new Date().getSeconds();
-  let ampm = "AM";
+let users = []
 
-  if (h > 12) {
-    h = h - 12;
-    ampm = "PM";
-  }
+document.addEventListener('DOMContentLoaded', async () => {
+    let response =  await fetch('/users.json');
+    let eredmeny = await response.json();
+    for (let e of eredmeny.users) {
+        users.push(e)
+    }
+    document.getElementById('osszes').addEventListener('click', async () => {
+        document.getElementById('lista').textContent = ""
+        response =  await fetch('/users.json');
+        eredmeny = await response.json();
+        users.sort((a, b) => a.lastName.localeCompare(b.lastName))
+        for (let e of users) {
+            let li = document.createElement('li')
+            li.innerHTML = e.lastName.toUpperCase() + "," + e.firstName;
+            document.getElementById('lista').appendChild(li)
+        }
+    })
+    document.getElementById('elerhetoseg').addEventListener('click', async () => {
+        document.getElementById('tablazat').textContent = ""
+        response =  await fetch('/users.json');
+        eredmeny = await response.json();
+        users.sort((a, b) => a.username.localeCompare(b.username))
+        for (let e of users) {
+            let tr = document.createElement('tr')
+            let td1 = document.createElement('td')
+            let td2 = document.createElement('td')
+            let td3 = document.createElement('td')
+            td1.textContent = e.username;
+            td2.textContent = e.email;
+            td3.textContent = e.phone;
+            tr.appendChild(td1)
+            tr.appendChild(td2)
+            tr.appendChild(td3)
+            document.getElementById('tablazat').appendChild(tr)
+        }
+    })
+    document.getElementById('cm').addEventListener('input', async () => {
+        let cm =  document.getElementById('cm').value
+        response =  await fetch('/users.json');
+        eredmeny = await response.json();
+        let userFilteredByHeight = users.filter(f => f.height >= cm)
+        let sum = 0;
+        for (let e of userFilteredByHeight) {
+            sum += e.weight
+        }
+        document.getElementById('oszsulyDisplay').textContent = cm + "cm-nél magasabbak öszsúlya: "+ sum +" kg"
+    })
+    document.getElementById('barnaSz').addEventListener('click', async () => {
+        response =  await fetch('/users.json');
+        eredmeny = await response.json();
+        let userFilteredByEyeColor = users.filter(f => f.eyeColor == "Brown")
+        document.getElementById('barnaszem').textContent = "Barnaszemüek száma: " + userFilteredByEyeColor.length
+        
+    })
 
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
-
-  hourEl.innerText = h;
-  minuteEl.innerText = m;
-  secondEl.innerText = s;
-  ampmEl.innerText = ampm;
-  setTimeout(() => {
-    updateClock();
-  }, 1000);
-}
-
-updateClock();
+})
